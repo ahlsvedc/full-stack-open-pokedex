ebab4363485e13055b3c668c66982e5fd6fa2af4
ahlsvedc <77110652+ahlsvedc@users.noreply.github.com>
2023-05-03 16:14:53 +0300

    removed /health for testing

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/health', (req, res) => {
-  // throw 'error...'
-  // // eslint-disable-next-line no-unreachable
-  res.send('ok')
-})
+// app.get('/health', (req, res) => {
+//   // throw 'error...'
+//   // // eslint-disable-next-line no-unreachable
+//   res.send('ok')
+// })
 
 app.get('/version', (req, res) => {
   res.send('17-branchtest') // change this string to ensure a new version deployed
